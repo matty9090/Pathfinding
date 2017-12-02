61e9814e6db154fc7d675ecd910585e9d6f3aacf
matty9090 <lowe.matthew@mail.com>
2017-12-02 18:40:09 +0000

    Menu code

--- StateMenu.cpp
+++ StateMenu.cpp
@@ -10,18 +10,52 @@ StateMenu::StateMenu(tle::I3DEngine *engine, Settings &settings) : State(engine,
 }
 
 void StateMenu::init() {
+	font		= engine->LoadFont("res/HoboStd.otf", 46U);
+	spr_logo	= engine->CreateSprite("res/logo.png", 314.0f, 76.0f);
+	spr_bg		= engine->CreateSprite("res/bg.jpg");
+
+	items[0] = { "Choose map", &StateMenu::action_select };
+	items[1] = { "Exit", &StateMenu::action_exit };
+
+	selected = 0;
 	
+	int i = 0;
+
+	for (auto &item : items) {
+		item.second.pos = Vec2<>(512, i * 56 + 380);
+		++i;
+	}
+
+	state_change = false;
+	return_state = State::Exit;
 }
 
 int StateMenu::run() {
-	while (engine->IsRunning()) {
+	while (engine->IsRunning() && !state_change) {
 		if (engine->KeyHit(Key_Escape)) {
 			engine->Stop();
 			break;
 		}
 
+		if (engine->KeyHit(Key_Down)) selected = (selected + 1) % items.size();
+		if (engine->KeyHit(Key_Up)) selected = (selected - 1) % items.size();
+
+		if (engine->KeyHit(Key_Return)) ((*this).*items[selected].action)();
+
+		for (auto &item : items)
+			font->Draw(item.second.txt, item.second.pos.x, item.second.pos.y, (item.first == selected) ? kBlue : kBlack, kCentre, kVCentre);
+
 		engine->DrawScene();
 	}
 
-	return State::Exit;
+	return return_state;
+}
+
+void StateMenu::action_select() {
+
+}
+
+void StateMenu::action_exit() {
+	state_change = true;
+	return_state = State::Exit;
 }
--- StateMenu.hpp
+++ StateMenu.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <TL-Engine.h>
-#include <vector>
 #include <map>
 #include <memory>
+#include <string>
+#include <vector>
 
 #include "Settings.hpp"
 #include "State.hpp"
@@ -16,5 +17,22 @@ class StateMenu : public State {
 		int run();
 
 	private:
-	
+		typedef void(StateMenu::*MenuAction)();
+
+		tle::IFont *font;
+		tle::ISprite *spr_bg;
+		tle::ISprite *spr_logo;
+
+		struct MenuItem {
+			std::string txt;
+			MenuAction action;
+			Vec2<> pos;
+		};
+
+		bool state_change;
+		int selected, return_state;
+		std::map<int, MenuItem> items;
+
+		void action_select();
+		void action_exit();
 };
